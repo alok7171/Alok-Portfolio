1af5c6603e0f81e53d2ea64193eeca36b0d8b048
alok7171 <alokbansode71@gmail.com>
2024-03-15 12:29:33 +0530

    Files Added

---
+++ data/story.js
@@ -0,0 +1,48 @@
+var story = {
+ "docName": "AlokPortfolio",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": false,
+ "galleryPageColorsEnabled": true,
+ "galleryDecorNodesEnabled": true,
+ "zoomEnabled": true,
+ "cloud": false,
+ "title": "AlokPortfolio",
+ "layersExist": true,
+ "pages": [
+  {
+   "id": "1:2",
+   "groupIndex": 0,
+   "title": "Final",
+   "image": "final.png",
+   "index": 0,
+   "width": 1920,
+   "height": 4320,
+   "x": -960,
+   "y": -2160,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [],
+   "layout": null,
+   "tmpSolitBackground": false
+  }
+ ],
+ "totalImages": 1,
+ "groups": [
+  {
+   "id": "0:1",
+   "index": 0,
+   "name": "Page 1",
+   "backColor": "#1E1E1E"
+  }
+ ]
+}
